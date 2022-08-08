f381c7d72c5ca22201e39ffcb51a5fba04267cf0
astridpe <71065172+astridpe@users.noreply.github.com>
2022-08-08 20:31:08 +0200

    Made a calculator and a BMI-caculator web-application with express.js.

--- Module19_express.js/server.js
+++ Module19_express.js/server.js
@@ -6,6 +6,10 @@ app.get("/", function(req, res){
     res.send("<h1>Hello, world!</h1>");
 });
 
+app.get("/contact", function(req, res){
+  res.send("Contact me at astridTesting@gmail.com");
+})
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
