95e7ad5ec0759cefd8ff67d66ef0e77fa0b9acb3
astridpe <71065172+astridpe@users.noreply.github.com>
2022-08-13 12:04:52 +0200

    Completed the Newsletter_signup-project.

--- Module20_APIs/newsletter_signup/app.js
+++ Module20_APIs/newsletter_signup/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -8,18 +10,50 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
-
 });
+
+mailchimp.setConfig(
+  {
+  apiKey: "",
+  server: ""
+  }
+);
 
 app.post("/", function(req, res){
 
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var email = req.body.email;
-  console.log(firstName, lastName, email);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const listId = ""
 
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+  };
+
+  async function run() {
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName
+      }
+    });
+
+    res.sendFile(__dirname + "/success.html")
+    console.log(`Successfully added contact as an audience member.
+      The contact's id is ${response.id}.`);
+  }
+
+  run().catch(e => res.sendFile(__dirname + "/failure.html"));
 });
 
-app.listen(3000, function(){
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server started on port 3000");
 });
