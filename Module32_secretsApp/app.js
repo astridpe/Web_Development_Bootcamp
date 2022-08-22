e76e32f93a3bad5603f2052f144a178937b8f0a0
astridpe <71065172+astridpe@users.noreply.github.com>
2022-08-22 18:44:16 +0200

    Adding salt to the hashed password and use the hash-function bcrypt instead of md5.

--- Module32_secretsApp/app.js
+++ Module32_secretsApp/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -37,16 +38,18 @@ app.route("/login")
 
 .post(function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
       console.log(err);
     } else {
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if (result === true){
+            res.render("secrets");
+          }
+        });
       }
     }
   });
@@ -59,17 +62,20 @@ app.route("/register")
 })
 
 .post(function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.passord)
-  });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.passord, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
