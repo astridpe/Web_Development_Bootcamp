460f0b7ceb9f65564192c5e78367340551dce8c1
astridpe <71065172+astridpe@users.noreply.github.com>
2022-08-26 09:11:49 +0200

    Complete the keeperApp with React components

---
+++ Module33_react.js/keeper-app-part-1/src/index.js
@@ -0,0 +1,12 @@
+
+//1. Create a new React app.
+import React from "react";
+import ReactDOM from "react-dom";
+//2. Create a App.jsx component.
+import App from "./components/App";
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
+
