7a1da69bb395acced814b9d92fa0ddf1864adb97
astridpe <71065172+astridpe@users.noreply.github.com>
2022-08-27 13:37:01 +0200

    Make the input and login parts in the html-form as individual components.

--- Module33_react.js/conditional-rendering/src/components/App.jsx
+++ Module33_react.js/conditional-rendering/src/components/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import Input from "./Input";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="container">
       <h1>Hello</h1>
       <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+      <Input 
+        type="text"
+        placeholder="Username"
+      />
+      <Input 
+        type="password"
+        placeholder="Password"
+        />
+      <Login 
+        type="submit"
+      />
       </form>
     </div>
   );
---
+++ Module33_react.js/conditional-rendering/src/components/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Login(props){
+    return(
+        <button type={props.type}>Login</button>
+    );
+}
+
+export default Login; 
