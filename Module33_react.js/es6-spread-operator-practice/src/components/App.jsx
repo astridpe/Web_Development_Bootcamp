694620adad1aa38f8442f19d04c571c6fd621535
Astrid Pettersen <s354534@oslomet.no>
2022-12-08 14:34:48 +0100

    give the toDoList-app functionality using state and the spread-operator

--- Module33_react.js/es6-spread-operator-practice/src/components/App.jsx
+++ Module33_react.js/es6-spread-operator-practice/src/components/App.jsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function App() {
+const [inputText, setInputText] = useState("");
+const [items, setItems] = useState([])
+
+function handleChange(event) {
+  const newValue  = event.target.value;
+  setInputText(newValue);
+}
+
+function handleClick() {
+  setItems(prevItems => {
+    return [...prevItems, inputText]
+  });
+}
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} value={inputText} type="text" />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+        {items.map((todoItem) => <li>{todoItem}</li>)} 
         </ul>
-      </div>
+        </div>
     </div>
   );
 }
